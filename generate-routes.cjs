a154ec9aec0cc2d8473115dc4cfcb816d8e1f83d
Keerthana C. <shreyasekar1210@gmail.com>
2026-09-25 22:26:50 +0530

    Clean up Cloudflare changes

--- generate-routes.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-const source = "dist/index.html";
-
-for (const route of ["cuddle-nest", "clario", "noir-crumb"]) {
-  fs.mkdirSync(`dist/${route}`, { recursive: true });
-  fs.copyFileSync(source, `dist/${route}/index.html`);
-}
